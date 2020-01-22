0922f9820119721eaab465e836fec08ea8dac67b
Peng Deng <peng.deng@informsoftware.com>
2020-01-22 14:34:57 +0800

    Add test for application context

---
+++ src/test/java/com/informsoftware/road/mock/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.informsoftware.road.mock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
