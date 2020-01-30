81442be784466b7d0b487ab464cfb6d722bcee8a
Peng Deng <peng.deng@informsoftware.com>
2020-01-30 21:22:13 +0800

    Customize theme

--- src/main/javascript/src/common/App.js
+++ src/main/javascript/src/common/App.js
@@ -4,7 +4,20 @@ import { loadEnums } from '../enums/enumsSlice';
 import EndPointsListContainer from '../endPoints/EndPointsListContainer'
 import EndPointEdit from '../endPoints/EndPointEdit'
 import FilterSorterBar from '../filters/FilterSorterBar'
-import AppBar from './AppBar'
+import AppBar from './AppBar';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+  palette: {
+    secondary: { main: "rgba(0, 145, 165, 1)" },
+    primary: { main: "rgba(0, 84, 110, 1)" },
+    error: { main: 'rgba(192, 2, 34, 1)' },
+    warning: { main: 'rgba(243, 131, 0, 1)' },
+    info: { main: 'rgba(0, 150, 210, 1)' },
+    success: { main: 'rgba(190, 203, 0, 1)' },
+    text: { primary: 'rgba(52, 64, 70, 1)' }
+  },
+});
 
 function App() {
   const dispatch = useDispatch();
@@ -14,12 +27,12 @@ function App() {
   }, [ dispatch ]);
 
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <AppBar />
       <FilterSorterBar />
       <EndPointsListContainer />
       <EndPointEdit />
-    </div>
+    </ThemeProvider>
   );
 }
 
